c2e25010681687b7cde9a4440f2f4a285513d416
kubilaycaglayan <kblycaglayan@gmail.com>
2020-11-02 12:49:58 +0300

    Add category database validation tests and create callback for order_in_kind

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  before_create :increase_order_in_kind
+
+  private
+  def increase_order_in_kind
+    return if Category.where(kind: self.kind).count < 1
+    self.order_in_kind = Category.where(kind: self.kind).maximum('order_in_kind') + 1
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   let(:product_1) { Product.create(code: 'WAT24480TR', definition: 'definition') }
   let(:category_1) { Category.create(kind: 'Bosch', definition: 'Washing Machines', order_in_kind: 1)}
+  let(:category_2) { Category.create(kind: 'Bosch', definition: 'Washing Machines')}
+  let(:category_info_1) { { kind: 'Bosch', definition: 'Washing Machines' } }
+  let(:category_info_2) { { kind: 'Muhasebe', definition: 'Washing Machines' } }
 
   describe 'database validations' do
     it 'successfully creates a record' do
@@ -13,16 +16,26 @@ RSpec.describe Category, type: :model do
       expect(category_1.save).not_to be false
     end
 
-    describe 'kind' do
-
-    end
-
-    describe 'definition' do
-
-    end
-
     describe 'order_in_kind' do
+      it 'defaults to 1' do
+        expect(category_2.order_in_kind).to be 1
+        expect(category_2.order_in_kind).not_to be 2
+      end
 
+      it 'automatically increases in a specific kind' do
+        category_1
+        expect(category_2.order_in_kind).to be 2
+        expect(category_2.order_in_kind).not_to be 1
+
+        expect(Category.create(category_info_1).order_in_kind).to be 3
+        expect(Category.create(category_info_1).order_in_kind).not_to be 3
+
+        expect(Category.create(category_info_1).order_in_kind).to be 5
+        expect(Category.create(category_info_1).order_in_kind).not_to be 5
+
+        expect(Category.create(category_info_2).order_in_kind).to be 1
+        expect(Category.create(category_info_2).order_in_kind).to be 2
+      end
     end
   end
 end
