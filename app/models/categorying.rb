3d2729f66c31d7c323964de0156e0f73c6382880
kubilaycaglayan <kblycaglayan@gmail.com>
2020-11-03 13:03:49 +0300

    Add tests category identifier class method

--- app/models/categorying.rb
+++ app/models/categorying.rb
@@ -32,11 +32,11 @@ class Categorying < ApplicationRecord
       when 'M'
         order_in_kind = 3
       when 'N'
-        return
+        order_in_kind = 0
       when 'P'
         order_in_kind = 2
       when 'Q'
-        return
+        order_in_kind = 0
       when 'R'
         order_in_kind = 1
       when 'S'
@@ -45,10 +45,11 @@ class Categorying < ApplicationRecord
         order_in_kind = 3
       when 'W'
         order_in_kind = 1
+      when /[0-9]/
+        order_in_kind = 6
       else
-        return
+        order_in_kind = 0
       end
-
       Categorying.create( product_id: product.id, category_id: Category.find_by(kind: 'Bosch', order_in_kind: order_in_kind).id)
     end
 
@@ -56,50 +57,38 @@ class Categorying < ApplicationRecord
       case product.code.first
       when 'A'
         order_in_kind = 8
-        return
       when 'B'
         order_in_kind = 9
-        return
       when 'D'
         order_in_kind = 6
-        return
       when 'G'
         order_in_kind = 3
-        return
       when 'H'
         order_in_kind = 4
-        return
       when 'K'
         order_in_kind = 3
-        return
       when 'L'
         order_in_kind = 7
-        return
       when 'M'
         order_in_kind = 8
-        return
       when 'N'
         order_in_kind = 8
-        return
       when 'P'
         order_in_kind = 5
-        return
       when 'Q'
-        return
+        order_in_kind = 0
       when 'R'
         order_in_kind = 8
-        return
       when 'S'
         order_in_kind = 2
-        return
       when 'T'
         order_in_kind = 8
-        return
       when 'W'
         order_in_kind = 1
-        return
+      when /[0-9]/
+        order_in_kind = 7
       else
-        return
+        order_in_kind = 0
       end
 
       Categorying.create( product_id: product.id, category_id: Category.find_by(kind: 'Muhasebe', order_in_kind: order_in_kind).id)
--- spec/models/categorying_spec.rb
+++ spec/models/categorying_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Categorying, type: :model do
   let(:product_1) { Product.create(code: 'WAT24480TR', definition: 'definition') }
+  let(:product_2) { Product.create(code: '43PFS5302', definition: 'definition') }
+  let(:product_3) { Product.create(code: 'BGB7330', definition: 'definition') }
+  let(:product_4) { Product.create(code: 'DWK65AD20R', definition: 'definition') }
+  let(:product_5) { Product.create(code: 'HGD745360T', definition: 'definition') }
+  let(:product_6) { Product.create(code: 'KGN56AIF0N', definition: 'definition') }
+  let(:product_7) { Product.create(code: 'MFW3520W', definition: 'definition') }
+  let(:product_8) { Product.create(code: 'HBN331S2T', definition: 'definition') }
+  let(:product_9) { Product.create(code: 'PBP6C2B80O', definition: 'definition') }
+  let(:product_10) { Product.create(code: 'SMS46IW00T', definition: 'definition') }
+
   let(:category_1) { Category.create(kind: 'Bosch', definition: 'Washing Machines', order_in_kind: 1)}
 
   let(:categorying_1) { Categorying.new(category_id: category_1.id, product_id: product_1.id) }
@@ -57,16 +67,91 @@ RSpec.describe Categorying, type: :model do
       end
 
       describe 'bosch category identifier' do
-        it 'identifies the category of a bosch kind product' do
+        it 'identifies the category of a bosch kind product - solo' do
           new_categorying = Categorying.bosch_category_identifier(product_1)
           expect(new_categorying.product_id).to be product_1.id
           expect(new_categorying.category_id).to be Category.where(kind: 'Bosch', definition: 'Solo').first.id
         end
 
-        it 'identifies the category of a bosch kind product' do
+        it 'identifies the category of a bosch kind product - solo' do
           new_categorying = Categorying.bosch_category_identifier(product_1)
           expect(new_categorying.product_id).to be product_1.id
           expect(new_categorying.category_id).to be Category.where(kind: 'Bosch', definition: 'Solo').first.id
+        end
+
+        it 'identifies the category of a bosch kind product - televizyon' do
+          new_categorying = Categorying.bosch_category_identifier(product_2)
+          expect(new_categorying.product_id).to be product_2.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Bosch', definition: 'Televizyon').first.id
+        end
+
+        it 'identifies the category of a bosch kind product - kea' do
+          new_categorying = Categorying.bosch_category_identifier(product_7)
+          expect(new_categorying.product_id).to be product_7.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Bosch', definition: 'Küçük Ev Aleti').first.id
+        end
+
+        it 'identifies the category of a bosch kind product - Ankastre' do
+          new_categorying = Categorying.bosch_category_identifier(product_5)
+          expect(new_categorying.product_id).to be product_5.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Bosch', definition: 'Ankastre').first.id
+        end
+      end
+
+      describe 'muhasebe category identifier' do
+
+        it 'identifies the category of a bosch kind product - Çamaşır Makinesi' do
+          new_categorying = Categorying.muhasebe_category_identifier(product_1)
+          expect(new_categorying.product_id).to be product_1.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Muhasebe', definition: 'Çamaşır Makinesi').first.id
+        end
+
+        it 'identifies the category of a bosch kind product - Bulaşık Makinesi' do
+          new_categorying = Categorying.muhasebe_category_identifier(product_10)
+          expect(new_categorying.product_id).to be product_10.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Muhasebe', definition: 'Bulaşık Makinesi').first.id
+        end
+
+        it 'identifies the category of a bosch kind product - Buzdolabı' do
+          new_categorying = Categorying.muhasebe_category_identifier(product_6)
+          expect(new_categorying.product_id).to be product_6.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Muhasebe', definition: 'Buzdolabı').first.id
+        end
+
+        it 'identifies the category of a bosch kind product - Fırın' do
+          new_categorying = Categorying.muhasebe_category_identifier(product_5)
+          expect(new_categorying.product_id).to be product_5.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Muhasebe', definition: 'Fırın').first.id
+        end
+
+        it 'identifies the category of a bosch kind product - Ocak' do
+          new_categorying = Categorying.muhasebe_category_identifier(product_9)
+          expect(new_categorying.product_id).to be product_9.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Muhasebe', definition: 'Ocak').first.id
+        end
+
+        it 'identifies the category of a bosch kind product - Davlumbaz' do
+          new_categorying = Categorying.muhasebe_category_identifier(product_4)
+          expect(new_categorying.product_id).to be product_4.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Muhasebe', definition: 'Davlumbaz').first.id
+        end
+
+        it 'identifies the category of a bosch kind product - Televizyon' do
+          new_categorying = Categorying.muhasebe_category_identifier(product_2)
+          expect(new_categorying.product_id).to be product_2.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Muhasebe', definition: 'Televizyon').first.id
+        end
+
+        it 'identifies the category of a bosch kind product - Küçük Ev Aleti' do
+          new_categorying = Categorying.muhasebe_category_identifier(product_7)
+          expect(new_categorying.product_id).to be product_7.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Muhasebe', definition: 'Küçük Ev Aleti').first.id
+        end
+
+        it 'identifies the category of a bosch kind product - Elektrikli Süpürge' do
+          new_categorying = Categorying.muhasebe_category_identifier(product_3)
+          expect(new_categorying.product_id).to be product_3.id
+          expect(new_categorying.category_id).to be Category.where(kind: 'Muhasebe', definition: 'Elektrikli Süpürge').first.id
         end
 
       end
