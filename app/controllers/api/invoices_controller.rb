52b44365290254c33d281a1bb9a9aa522426f9ef
kubilaycaglayan <kblycaglayan@gmail.com>
2020-11-17 13:52:55 +0300

    Add permitted parameters to the api/invoices controller

--- app/controllers/api/invoices_controller.rb
+++ app/controllers/api/invoices_controller.rb
@@ -3,19 +3,51 @@ class Api::InvoicesController < ApplicationController
   before_action :authenticate_with_api_key
 
   def create
+    if invoice_params['data'].nil?
+      render json: {
+        message: 'No record provided.'
+      }
+      return
+    end
+
+    result = Invoice.insert_all(invoice_params['data'], returning: %w[product_code definition])
+
     render json: {
-      autenthicated: true
+      autenthicated: true,
+      result: result
     }
   end
 
   private
 
   def authenticate_with_api_key
-    unless params[:api_key] == 'api_key'
+    unless api_key_params['api_key'] == '123456'
       render json: {
         autenthicated: false,
         message: 'Wrong API key.'
       }
+      return
     end
+  end
+
+  def api_key_params
+    params.permit(:api_key)
+  end
+
+  def invoice_params
+    params.permit(:data => %i[
+                invoice_number
+                pen_number
+                quantity
+                tax_kdv
+                tax_otv
+                definition
+                value_date
+                invoice_date
+                sum
+                product_code
+                created_at
+                updated_at
+              ])
   end
 end
--- spec/requests/api/invoices_request_spec.rb
+++ spec/requests/api/invoices_request_spec.rb
@@ -6,28 +6,38 @@ RSpec.describe 'Api::Invoices', type: :request do
   let(:api_key_wrong) { 'api_key_wrong' }
 
   describe 'POST' do
-    it 'request with the right API key' do
-      post "#{proxy}/invoices",
-           params: { api_key: api_key }
-      expect(JSON[response.body]['autenthicated']).to be true
+    describe 'is protected with a private API key' do
+      it 'request with the correct API key' do
+        post "#{proxy}/invoices",
+             params: { api_key: api_key }
+        expect(JSON[response.body]['autenthicated']).to be true
+      end
+
+      it 'request with the correct API key - falsy' do
+        post "#{proxy}/invoices",
+             params: { api_key: api_key }
+        expect(JSON[response.body]['autenthicated']).not_to be false
+      end
+
+      it 'request with the wrong API key' do
+        post "#{proxy}/invoices",
+             params: { api_key: api_key_wrong }
+        expect(JSON[response.body]['autenthicated']).to be false
+      end
+
+      it 'request with the wrong API key - falsy' do
+        post "#{proxy}/invoices",
+             params: { api_key: api_key_wrong }
+        expect(JSON[response.body]['autenthicated']).not_to be true
+      end
     end
 
-    it 'request with the right API key - falsy' do
-      post "#{proxy}/invoices",
-           params: { api_key: api_key }
-      expect(JSON[response.body]['autenthicated']).not_to be false
-    end
-
-    it 'request with the wrong API key' do
-      post "#{proxy}/invoices",
-           params: { api_key: api_key_wrong }
-      expect(JSON[response.body]['autenthicated']).to be false
-    end
-
-    it 'request with the wrong API key - falsy' do
-      post "#{proxy}/invoices",
-           params: { api_key: api_key_wrong }
-      expect(JSON[response.body]['autenthicated']).not_to be true
+    describe 'invoice records' do
+      it 'creates new invoice records' do
+        post "#{proxy}/invoices",
+          params: { api_key: api_key }
+        expect
+      end
     end
   end
 end
